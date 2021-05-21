4278d5d77f15cdd10628f2ecfdcf51b1e0fecc66
FrederkVinter <frederik.vinterhviid@gmail.com>
2021-05-21 13:47:52 +0200

    cases

--- wire_cutting/test/run_tests.sh
+++ wire_cutting/test/run_tests.sh
@@ -3,8 +3,8 @@
 source ../../../devel/setup.bash
 
 #declare -a planners=("j" "j_d" "jp" "jp_d" "jpv" "jpv_d" "jpva" "jpva_d")
-declare -a planners=("jpva")
-declare -a programs=("rectangle.txt" "stairs.txt" "cone.txt" "sweep.txt")
+declare -a planners=("d_j" "d_jp" "d_jpv" "d_jpva")
+declare -a programs=("rectangle.txt" "cone.txt" "saddel.txt")
 
 for i in "${planners[@]}"
 do
